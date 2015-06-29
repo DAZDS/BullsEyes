6bf6478091c1f32d1c5e622ab971f92441881c9a
Damian Stepien <dstepien@MacBook-Pro-Damian.local>
2015-06-29 16:52:11 +0200

    Initial Commit

---
+++ BullsEyes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEyes
+//
+//  Created by Damian Stepien on 29.06.2015.
+//  Copyright (c) 2015 Damian Stepien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
